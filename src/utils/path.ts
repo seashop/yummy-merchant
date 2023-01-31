5eb4c29349026a7034d92d000f70557711617804
unknown <niujie@cnegroup.com>
2023-01-31 23:02:21 +0800

    更新path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,10 +1,10 @@
 const path = {
   mobile: {
     login: ':merchant/passport',
-    getCategoryList: '/inns/{innId}/productCats',
-    getProductList: '/inns/{innId}/products:list',
+    getCategoryList: ':merchant/inns/{innId}/productCats',
+    getProductList: ':merchant/inns/{innId}/products:list',
     getImgUrl: ':common/images/{id}/url',
-    createOrder: '/inns/{innId}/orders:place',
+    createOrder: ':merchant/inns/{innId}/orders:place',
     getOrderDetail: ':merchant/inns/{innId}/orders/{orderId}',
     getOrderList: ':merchant/inns/{innId}/orders',
     updateOrder: ':merchant/inns/{innId}/orders/{orderId}',
